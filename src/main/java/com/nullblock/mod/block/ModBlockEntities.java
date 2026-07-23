7ce5bfbe2f2bded9a4b54b8766938a1df027afa3
kintil555 <dzikriganteng52637@gmail.com>
2026-07-23 16:35:04 +0700

    Add files via upload

--- src/main/java/com/nullblock/mod/block/ModBlockEntities.java
+++ src/main/java/com/nullblock/mod/block/ModBlockEntities.java
@@ -7,13 +7,15 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.Set;
+
 public class ModBlockEntities {
 
     public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITIES =
             DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, NullBlockMod.MODID);
 
     public static final RegistryObject<BlockEntityType<NullBlockEntity>> NULL_BLOCK_ENTITY =
-            BLOCK_ENTITIES.register("null_block_entity", () -> BlockEntityType.Builder.of(
-                    NullBlockEntity::new, ModBlocks.NULL_BLOCK.get()
-            ).build(null));
+            BLOCK_ENTITIES.register("null_block_entity", () -> new BlockEntityType<>(
+                    NullBlockEntity::new, Set.of(ModBlocks.NULL_BLOCK.get())
+            ));
 }
